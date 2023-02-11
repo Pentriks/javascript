084d465f63339ec98431fd24d511fd3ecbe1c571
Endrik Santana Garcia da Costa <116817681+Pentriks@users.noreply.github.com>
2023-02-11 13:59:32 -0300

    Exercício 008 e 009 JS

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você passou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segunça!`)
