02c5597648ff7bc44a8535fafcf57d443411cc0e
Endrik Santana Garcia da Costa <116817681+Pentriks@users.noreply.github.com>
2023-02-16 11:54:46 -0300

    Atualizei exercício 018

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -11,7 +11,7 @@ function isNumero(n) {
     }
 }
 
-function inLista(n, 1) {
+function inLista(n, l) {
     if (l.indexOf(Number(n)) != -1) {
         return true
     } else {
@@ -35,7 +35,7 @@ function adicionar() {
 
 function finalizar() {
     if(valores.length == 0) {
-        window.alert('Adicione valores antes de finalizar!')
+        window.alert(`Adicione valores antes de finalizar!`)
     } else {
         let tot = valores.length
         let maior = valores[0]
@@ -45,16 +45,16 @@ function finalizar() {
         for(let pos in valores) {
             soma += valores[pos]
             if (valores[pos] > maior)
-            maior = valores[pos]
-            if(valores[pos] < menor)
-            menor = valores[pos]
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
         }
         media = soma / tot
         res.innerHTML = ''
         res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
         res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
         res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
-        res.innerHTML += `<p>Somando todos os valores, temos${soma}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
         res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
     }
 }
